bfa1542b9f72e64229b0f55dc7561e8bad8238c1
Jai0212 <jj.joshijai@gmail.com>
2024-11-02 20:13:11 -0400

    fix(font-size on mobile devices for courses): Deck.jsx

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -155,7 +155,7 @@ const DeckProject = ({
             <h2>{items[i].title}</h2>
             {items[i].id === "internships" || items[i].id === "projects" ? <p dangerouslySetInnerHTML={{ __html: items[i].display.replace(/\n/g, '<br>') }} style={{ fontStyle: 'italic', marginBottom: '-10px' }} /> : <></>}
             {items[i].id === "tools" || items[i].id === "aboutMe" || (items[i].id === "projects" && items[i].centered && isNotMobile) || (items[i].id === "internships" && items[i].centered && isNotMobile) ?
-              <p dangerouslySetInnerHTML={{ __html: items[i].description.replace(/\n/g, '<br>') }} style={{ fontSize: !isNotMobile && items[i].className === 'about-me-description' ? '10px' : '14px' }} /> : <></>}
+              <p dangerouslySetInnerHTML={{ __html: items[i].description.replace(/\n/g, '<br>') }} style={{ fontSize: !isNotMobile && (items[i].className === 'about-me-description' || items[i].className === "harvard-courses-image" || items[i].className === "google-courses-image" || items[i].className === "aws-courses-image") ? '10px' : '14px' }} /> : <></>}
             {items[i].title === 'My Links' ?
               <div>
                 <a href="https://github.com/Jai0212" target="_blank" rel="noopener noreferrer" className={items[i].centered && isNotMobile ? '' : 'disabled'}>
